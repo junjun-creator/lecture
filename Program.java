51c68e14f1450bc23c07213776966a1f498c01e0
BYEONGJUN KIM(JUN) <65852909+junjun-creator@users.noreply.github.com>
2020-09-02 19:07:10 +0900

    20200902 study
    20200902 study

---
+++ Program.java
@@ -0,0 +1,16 @@
+public class Program{
+	public static void main(String[] args){
+
+		int kor1,kor2,kor3,total;
+		double avg;
+
+		kor1 = 50;
+		kor2 = 60;
+		kor3 = 80;
+
+		total = kor1+kor2+kor3;
+		avg = total/3;
+		//System.out.println(avg);
+		
+	}
+}
