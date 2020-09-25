3e0f26b5f44f42fb14e09500ccc04f304f21c8d5
BYEONGJUN KIM(JUN) <65852909+junjun-creator@users.noreply.github.com>
2020-09-25 23:10:59 +0900

    20200925 study
    20200925 study

---
+++ 0925/chapter7ex4/Program.java
@@ -0,0 +1,25 @@
+package chapter7ex4;
+
+public class Program {
+	public static void main(String[] args) {
+		/*
+		GoBoard board = new GoBoard();
+		GoContainer blackContainer = new GoContainer("Black");
+		GoContainer whiteContainer = new GoContainer("White");
+		OperationBoard ob = new OperationBoard();
+		Computer com = new Computer();
+		
+		//게임 시작
+		System.out.println("게임을 시작합니다. 흑돌이 우선입니다.");
+		board.print();
+		ob.print(blackContainer.getCount(),whiteContainer.getCount());
+		
+		
+		//흑돌(컴퓨터 턴)
+		blackContainer.pickStone();
+		com.setXY();
+		board.setStone(1, 1, blackContainer.getColor());
+		board.print();
+		ob.print(blackContainer.getCount(),whiteContainer.getCount());*/
+	}
+}
